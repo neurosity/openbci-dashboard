56f9d91560aa1a9d3e683c5812f431f8d0c38759
andrewheusser <andrew.heusser@gmail.com>
2016-04-23 11:30:17 -0400

    topoplot works!
    -coordinates are are rough estimates.  may want to change this, or make
    them user definable.
    
    -colors can be customized as well..using chroma to map from numbers to
    color values

--- app/components/topo/topo.js
+++ app/components/topo/topo.js
@@ -7,19 +7,18 @@ angular.module('bciDashboard')
             $ctrl.getClass = function(index){
               return 'topoplot-u' + index
             };
-            $ctrl.getColor = function(pixel,index){
-              // var pixel_mapped = 255
-              return {'background-color': 'rgb(' + (index*2) + ',0,0)'}
+            $ctrl.getColor = function(index,pixel,grid){
+              var min = Math.min.apply(Math,grid)
+              var max = Math.max.apply(Math,grid)
+              var f = chroma.scale('Spectral').domain([min,max]);
+              return {'background-color': f(pixel)}
 
             };
 
-            // $ctrl.series = ['Channel 1','Channel 2','Channel 3','Channel 4','Channel 5','Channel 6','Channel 7','Channel 8'];
             socket.on('bci:topo', function (data) {
                 $timeout(function () {
                     $ctrl.grid = [].concat.apply([], data.data);
-                    // console.log($ctrl.grid);
-                    // $ctrl.labels = data.labels;
-                    // $ctrl.data = data.data;
+
                 });
             });
         }
