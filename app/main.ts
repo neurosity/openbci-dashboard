0bb8f1c4fe336338275569e91dfe466bfd15ab87
Alex Castillo <alex@castillo.io>
2016-04-22 21:35:05 -0400

    enable prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,8 @@
 
 import { bootstrap } from 'angular2/platform/browser'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { enableProdMode } from 'angular2/core';
+
+enableProdMode();
 
 bootstrap(DashboardComponent);
