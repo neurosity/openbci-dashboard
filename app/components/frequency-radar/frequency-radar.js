b63405d3b3c87d8dda1596b44993e4ef2d25aed4
Alex Castillo <alex@castillo.io>
2016-04-29 23:17:23 -0400

    add frequency radar chart

---
+++ app/components/frequency-radar/frequency-radar.js
@@ -0,0 +1,47 @@
+
+angular.module('bciDashboard')
+    .component('bciFrequencyRadar', {
+        templateUrl: 'components/frequency-radar/frequency-radar.html',
+        bindings: {
+          eventName: '@'
+        },
+        controller: function ($timeout) {
+
+            var $ctrl = this;
+
+            var socket = io();
+
+            $ctrl.colors = [
+                { strokeColor: 'rgba(112,185,252,1)' },
+                { strokeColor: 'rgba(116,150,161,1)' },
+                { strokeColor: 'rgba(162,86,178,1)' },
+                { strokeColor: 'rgba(144,132,246,1)' },
+                { strokeColor: 'rgba(138,219,229,1)' },
+                { strokeColor: 'rgba(232,223,133,1)' },
+                { strokeColor: 'rgba(148,159,177,1)' },
+                { strokeColor: 'rgba(182,224,53,1)' }
+            ];
+
+            $ctrl.options = {
+                responsive: true,
+                animation: true,
+                animationSteps: 15
+            };
+
+            $ctrl.series = ['Channel 1','Channel 2','Channel 3','Channel 4','Channel 5','Channel 6','Channel 7','Channel 8'];
+
+            socket.on($ctrl.eventName, function (data) {
+                $timeout(function () {
+                    $ctrl.data = data.data;
+                    $ctrl.labels = data.labels.map(function (label, i) {
+                        return i % 10 === 0 ? label : '';
+                    });
+                });
+            });
+
+            $ctrl.$onDestroy = function () {
+                socket.removeListener($ctrl.eventName);
+            };
+
+        }
+    });
